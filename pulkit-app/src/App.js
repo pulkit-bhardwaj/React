2d68625668c8da9c0c0dbe25b6679c5d262cd415
Pulkit Bhardwaj <pulkitbhardwaj3380@gmail.com>
2022-08-16 15:33:32 -0500

    State use to convert lowercase to uppercase.

--- pulkit-app/src/App.js
+++ pulkit-app/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 function App() {
   return (
     <>
 <Navbar title="Pulkit Project" aboutText="About us"/>
 {/* <Navbar/> */}
+<div className="container my-3">
+  <TextForm heading="Enter the Text to analyze below"/>
+</div>
     </>
   );
 }
